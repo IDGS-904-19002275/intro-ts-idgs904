c52ccd446e310c87b04664bba101013f4d2ccb21
Soulz <19002275@alumnos.utleon.edu.mx>
2023-06-09 20:40:07 -0600

    JJML-09/06/2023

---
+++ src/intro-ts/05-clases.ts
@@ -0,0 +1,42 @@
+class Persona2{
+    nombre:string='2';
+    edad:number=2;
+    constructor(nombre:string, edad:number){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    imprimir(){
+        console.log(`Nombre ${this.nombre} y  Edad: ${this.edad}`);
+        
+    }
+}
+
+let persona:Persona2
+persona=new Persona2('Juan', 27)
+persona.imprimir();
+
+
+//Modificadores de acceso
+
+class Dado{
+    private valor:number=0;
+    public tirar(){
+        this.generar();
+    } 
+
+private generar(){
+this.valor=Math.floor(Math.random()*6)+1
+}
+
+    public imprimir (){
+        console.log('valor');
+        
+    }
+
+}
+
+let dado1= new Dado();
+dado1.tirar();
+dado1.imprimir();
+
+
---
+++ src/intro-ts/06-herencia.ts
@@ -0,0 +1,8 @@
+class Prsona5{
+    protected nombre:string;
+    protected edad:number;
+    constructor(nombe:string, edad:number){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+}
